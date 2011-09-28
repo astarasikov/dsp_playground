d4969737e300502b8e1d4a73f4445f150cb2c2e5
Alexander Tarasikov <alexander.tarasikov@gmail.com>
2011-09-28 19:09:08 +0400

    Split examples into separate functions

--- main.cc
+++ main.cc
@@ -16,22 +16,7 @@ static void dump(const T& x) {
 	cout << "]" << endl;
 }
 
-int main() {
-	vector<double> f = {0, 1, 2, 3, 4};
-	vector<double> g = {0, 0, 0, 1, 0};
-	vector<double> res (f.size(), 0);
-	convolve1D(f, g, res);
-	dump(res);
-	vector<double> circ (f.size(), 0);
-	convolveCircular1D(f, g, circ);
-	dump(circ);
-
-	vector<double> sig = {1, 2, 3, 1, 3, 3, 4, 5};
-	vector<double> pat = {1, 2, 3, 1, 3, 3, 4, 5};
-	vector<double> cor (sig.size(), 0);
-	correlateCircular1D(pat, sig, cor);
-	dump(cor);
-
+static void test_fft_1d(void) {
 	vector<complex<double>> ff2 = {0, 1, 2, 3, 4, 5, 6, 7};
 	fft(ff2, false);
 	cout << "after FFT" << endl;
@@ -39,7 +24,9 @@ int main() {
 	fft(ff2, true);
 	cout << "after IFFT" << endl;
 	dump(ff2);
+}
 
+static void test_fft_2d(void) {
 	complex<double> arr[] = {
 		1, 2, 3, 4,
 		5, 6, 7, 8,
@@ -59,6 +46,35 @@ int main() {
 	}
 	cout << "2d fft" << endl;
 	dump(arr);
+}
+
+static void test_convolution_1d(void) {
+	vector<double> f = {0, 1, 2, 3, 4};
+	vector<double> g = {0, 0, 0, 1, 0};
+	vector<double> res (f.size(), 0);
+	convolve1D(f, g, res);
+	cout << "1d convolution" << endl;
+	dump(res);
+	vector<double> circ (f.size(), 0);
+	convolveCircular1D(f, g, circ);
+	cout << "1d circular convolution" << endl;
+	dump(circ);
+}
+
+static void test_correlation_1d(void) {
+	vector<double> sig = {1, 2, 3, 1, 3, 3, 4, 5};
+	vector<double> pat = {1, 2, 3, 1, 3, 3, 4, 5};
+	vector<double> cor (sig.size(), 0);
+	cout << "1d circular correlation" << endl;
+	correlateCircular1D(pat, sig, cor);
+	dump(cor);
+}
+
+int main() {
+	test_convolution_1d();
+	test_correlation_1d();
+	test_fft_1d();
+	test_fft_2d();
 
 	return 0;
 }
