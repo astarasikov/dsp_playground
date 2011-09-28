7ea43d4edbfa0691be6b809dee4f3612160d6643
Alexander Tarasikov <alexander.tarasikov@gmail.com>
2011-09-28 19:01:01 +0400

    Add an fft function with variable stride to allow specifying skip length to work on multiple dimensions

--- fft.hh
+++ fft.hh
@@ -69,4 +69,16 @@ void fft(std::vector<std::complex<T>> &arr, bool inverse) {
 	}
 }
 
+template <class T>
+void fft_real(std::complex<T> data[], size_t stride, size_t count, bool inverse) {
+	std::vector<std::complex<T>> vec(count, 0);
+	for (size_t i = 0; i < count; i++) {
+		vec[i] = data[stride * i];
+	}
+	fft(vec, inverse);
+	for (size_t i = 0; i < count; i++) {
+		data[stride * i] = vec[i];
+	}
+}
+
 #endif
